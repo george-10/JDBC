2a1171c1a396d25630cbdeec4864203361694b80
georges-10 <george.fayssal@gmail.com>
2024-01-30 22:36:10 +0200

    finalisation du devoir

---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+1-J'ai commencé par cree une DB sur un serveur. Cette base de données est hébergé par un service nommé Aiven.
+2-J'ai importé la librairie Sql de java.
+3-Pour etablir une connection, j'ai installé le driver de mySql. De plus j'ai besoin de l'URL, le username et le password qui dont tous donné par Aiven.
+4-j'établi la connection avec la base donne avec la methode DriverManager.getConnection().
+5-Pour pouvoir executer des commandes je cree un objet statement a partir de l'objet connection cree auparavant.
+6-Je sauvegarde ma commande dans un string.
+7-Enfin j'utilise executeUpdate() pour executer la requete SQL.
+8-Dans le cas de le requete "Select" on utilise l'objet resultSet pour sauvegarder la reponse de la requete et ainsi pour afficher toutes les rangés obtenues.
--- src/jdbc/main.java
+++ src/jdbc/main.java
@@ -3,7 +3,6 @@ import java.sql.*;
 
 public class main {
     public static void main(String[] args) {
-        // JDBC URL, username, and password
         String jdbcUrl = "jdbc:mysql://avnadmin:AVNS_5pZhuYE1OWJOMzdy61m@mysql-3775690f-george-cf4d.a.aivencloud.com:13427/defaultdb?ssl-mode=REQUIRED";
         String username = "avnadmin";
         String password = "AVNS_5pZhuYE1OWJOMzdy61m";
@@ -11,13 +10,21 @@ public class main {
         try {
             Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
             Statement statement = connection.createStatement();
-            String createTableSQL = "CREATE TABLE my_table (" +
-                    "id INT AUTO_INCREMENT PRIMARY KEY," +
-                    "name VARCHAR(255) NOT NULL" +
-                    ")";
-            statement.executeUpdate(createTableSQL);
-            System.out.println("Table created successfully.");
+            String insertSQL = "INSERT INTO students (name, code, address, age) " +
+                    "VALUES ('Jad', '51623', 'Tripoli', 52)";
+            statement.executeUpdate(insertSQL);
 
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM students");
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                String code = resultSet.getString("code");
+                String address = resultSet.getString("address");
+                int age = resultSet.getInt("age");
+
+                System.out.println("Name: " + name + ", Code: " + code + ", Address: " + address + ", Age: " + age);
+            }
+
+            resultSet.close();
             statement.close();
             connection.close();
         } catch (SQLException e) {
